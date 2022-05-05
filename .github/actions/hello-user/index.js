81a4dfdd099d647774b56442ea2afb4d9a54eada
Jheel Bharambe <87069669+jheelbharambe@users.noreply.github.com>
2022-05-05 14:19:47 +0530

    Update index.js

--- .github/actions/hello-user/index.js
+++ .github/actions/hello-user/index.js
@@ -7,11 +7,11 @@ try {
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
+  const dictionary = core.getInput('dictionary');
+  console.log(`Dictionary ${dictionary}!`);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
-  const dictionary = core.getInput('dictionary');
-  console.log(`Dictionary ${dictionary}!`);
 } catch (error) {
   core.setFailed(error.message);
 }
